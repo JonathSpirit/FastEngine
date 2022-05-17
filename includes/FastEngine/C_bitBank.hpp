6c9598828aed19c04bb7f605f1c4c36e5f9c2ece
GuillaumeG <48102745+JonathSpirit@users.noreply.github.com>
2022-05-17 18:34:54 +0200

    Documentation for C_bitBank.hpp

--- includes/FastEngine/C_bitBank.hpp
+++ includes/FastEngine/C_bitBank.hpp
@@ -6,6 +6,13 @@
 namespace fge
 {
 
+/**
+ * \class BitBank
+ * \ingroup utility
+ * \brief A class to manage a group of bits.
+ *
+ * \tparam TNbytes The number of bytes in the bank.
+ */
 template<std::size_t TNbytes>
 class BitBank
 {
@@ -14,15 +21,56 @@ public:
     BitBank() = default;
     ~BitBank() = default;
 
+    /**
+     * \brief Clear the bank.
+     *
+     * This function clear the bank by setting all bits to 0.
+     */
     void clear();
 
+    /**
+     * \brief Set a bit to 1 or 0.
+     *
+     * This function set the bit at the specified index to 1 or 0.
+     *
+     * \param index The index of the bit to set.
+     */
     void set(std::size_t index, bool flag);
+    /**
+     * \brief Get the value of a bit.
+     *
+     * This function get the value of the bit at the specified index.
+     *
+     * \param index The index of the bit to get.
+     * \return The value of the bit or 0 if out of range.
+     */
     [[nodiscard]] bool get(std::size_t index) const;
+    /**
+     * \brief Get the specified byte.
+     *
+     * \param index The index of the byte to get.
+     * \return The wanted byte or 0 if out of range.
+     */
     [[nodiscard]] uint8_t getByte(std::size_t index) const;
 
+    /**
+     * \brief Get the number of bytes in the bank.
+     *
+     * \return The number of bytes in the bank.
+     */
     [[nodiscard]] std::size_t getSize() const;
 
+    /**
+     * \brief Pack the bank data into the packet.
+     *
+     * \param pck The network packet to pack the bank data into.
+     */
     void pack(fge::net::Packet& pck);
+    /**
+     * \brief Unpack the bank data from the packet.
+     *
+     * \param pck The network packet to unpack the bank data from.
+     */
     void unpack(fge::net::Packet& pck);
 
 private:
