9eb0427d815c93ad0b98e44c873559ef43e7acbe
GuillaumeG <48102745+JonathSpirit@users.noreply.github.com>
2022-05-17 19:21:45 +0200

    Documentation on C_flag.hpp

--- includes/FastEngine/C_flag.hpp
+++ includes/FastEngine/C_flag.hpp
@@ -4,6 +4,13 @@
 namespace fge
 {
 
+/**
+ * \class Flag
+ * \ingroup utility
+ * \brief A class to handle flags
+ *
+ * A flag will be set to \b true only once, and wait for the input to be \b false before being set again.
+ */
 class Flag
 {
 public:
@@ -11,6 +18,14 @@ public:
         g_flag(defaultValue)
     {}
 
+    /**
+     * \brief Check the input and return the flag value
+     *
+     * This function will only return \b true once, and wait for the input to be \b false before being set again.
+     *
+     * \param input The boolean input to check
+     * \return The flag value
+     */
     inline bool check(bool input)
     {
         if (!this->g_flag)
@@ -25,20 +40,41 @@ public:
         return false;
     }
 
+    /**
+     * \brief Manually set the flag value
+     *
+     * \param value The boolean value to set
+     */
     inline void set(bool value)
     {
         this->g_flag = value;
     }
-    inline bool get() const
+    /**
+     * \brief Get the flag value
+     *
+     * \return The flag value
+     */
+    [[nodiscard]] inline bool get() const
     {
         return this->g_flag;
     }
 
+    /**
+     * \brief Manually set the flag value operator
+     *
+     * \param value The boolean value to set
+     * \return The flag value
+     */
     inline bool operator=(bool value)
     {
         return this->g_flag = value;
     }
 
+    /**
+     * \brief Get the flag value operator
+     *
+     * \return The flag value
+     */
     inline operator bool() const
     {
         return this->g_flag;
