7ff0e5c0946915024d12fb3188bca264accd9fdd
GuillaumeG <48102745+JonathSpirit@users.noreply.github.com>
2022-06-14 23:34:12 +0200

    Add license

--- includes/FastEngine/C_packet.hpp
+++ includes/FastEngine/C_packet.hpp
@@ -1,5 +1,28 @@
+/*
+ * Copyright 2022 Guillaume Guillet
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #ifndef _FGE_C_PACKET_HPP_INCLUDED_
 #define _FGE_C_PACKET_HPP_INCLUDED_
+
+/*
+ * Original from : https://github.com/SFML/SFML
+ * Copyright (C) 2007-2022 Laurent Gomila
+ *
+ * Altered/Modified by Guillaume Guillet
+ */
 
 #include <FastEngine/fastengine_extern.hpp>
 #include <FastEngine/C_matrix.hpp>
--- includes/FastEngine/C_packetBZ2.hpp
+++ includes/FastEngine/C_packetBZ2.hpp
@@ -1,16 +1,30 @@
 /*
-    Using the libbzip2 :
-
-    This program, "bzip2", the associated library "libbzip2", and all
-    documentation, are copyright (C) 1996-2019 Julian R Seward.  All
-    rights reserved.
-*/
+ * Copyright 2022 Guillaume Guillet
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #ifndef _FGE_C_PACKETBZ2_HPP_INCLUDED
 #define _FGE_C_PACKETBZ2_HPP_INCLUDED
 
 #include <FastEngine/fastengine_extern.hpp>
 #include <FastEngine/C_packet.hpp>
+
+/*
+ * This file is using the library :
+ * libbzip2
+ * copyright (C) 1996-2019 Julian R Seward.
+ */
 
 #define FGE_PACKETBZ2_DEFAULT_WORKFACTOR 0
 #define FGE_PACKETBZ2_DEFAULT_BLOCKSIZE 4
