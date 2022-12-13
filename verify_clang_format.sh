c398e0d14453e9de072e8b4a8842e3dee07822d1
GuillaumeG <48102745+JonathSpirit@users.noreply.github.com>
2022-12-13 14:40:12 +0100

    Add workflow to check good code format

---
+++ verify_clang_format.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+
+clang-format --style=file --Werror --verbose -n -i ./sources/*
+clang-format --style=file --Werror --verbose -n -i ./sources/extra/*
+clang-format --style=file --Werror --verbose -n -i ./sources/manager/*
+clang-format --style=file --Werror --verbose -n -i ./sources/object/*
+clang-format --style=file --Werror --verbose -n -i ./sources/private/*
+
+clang-format --style=file --Werror --verbose -n -i ./includes/FastEngine/*
+clang-format --style=file --Werror --verbose -n -i ./includes/FastEngine/extra/*
+clang-format --style=file --Werror --verbose -n -i ./includes/FastEngine/manager/*
+clang-format --style=file --Werror --verbose -n -i ./includes/FastEngine/object/*
+clang-format --style=file --Werror --verbose -n -i ./includes/private/*
+
+find examples/ -iname *.hpp -o -iname *.cpp -o -iname *.inl | xargs clang-format --style=file --Werror --verbose -n -i
