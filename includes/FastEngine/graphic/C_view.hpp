b1fe32aa6c5033431117ae20cd7ee1fef1348c77
GuillaumeG <48102745+JonathSpirit@users.noreply.github.com>
2023-02-16 15:32:28 +0100

    View: documentation

--- includes/FastEngine/graphic/C_view.hpp
+++ includes/FastEngine/graphic/C_view.hpp
@@ -27,12 +27,19 @@
 #include "FastEngine/fastengine_extern.hpp"
 #include "FastEngine/C_rect.hpp"
 #include "FastEngine/C_vector.hpp"
-#include <FastEngine/vulkan/C_viewport.hpp>
-#include <glm/glm.hpp>
+#include "FastEngine/vulkan/C_viewport.hpp"
+#include "glm/glm.hpp"
 
 namespace fge
 {
 
+/**
+ * \class View
+ * \ingroup graphics
+ * \brief Define a camera in a 2D scene
+ *
+ * A view is a 2D camera that defines what part of the 2D scene is visible.
+ */
 class FGE_API View
 {
 public:
@@ -43,32 +50,81 @@ public:
     void setCenter(const Vector2f& center);
     const Vector2f& getCenter() const;
 
+    /**
+     * \brief Set the size of the view
+     *
+     * A size smaller than the target will display a zoomed area,
+     * while a size greater than the target will show a bigger area.
+     *
+     * \param size New size of the view
+     */
     void setSize(const Vector2f& size);
     const Vector2f& getSize() const;
 
     void setRotation(float angleDeg);
     float getRotation() const;
 
+    /**
+     * \brief Set the viewport rectangle of the view
+     *
+     * The viewport is the rectangle into which the contents of the view are displayed,
+     * expressed as a factor (between 0 and 1) of the size of the RenderTarget to which the view is applied.
+     *
+     * For example, a view which takes the left side of the target would be defined by:
+     * \code view.setViewport({{0.0f, 0.0f}, {0.5f, 1.0f}}); \endcode
+     *
+     * \param factorViewport New viewport rectangle, expressed as a factor of the render-target's size
+     */
     void setFactorViewport(const fge::RectFloat& factorViewport);
     const fge::RectFloat& getFactorViewport() const;
 
+    /**
+     * \brief Reset the view to the given viewport
+     *
+     * The size and center of the view are adjusted so that the entire viewport is visible.
+     * The rotation is reset to 0.
+     *
+     * \param viewport New viewport to apply to the view
+     */
     void reset(const fge::vulkan::Viewport& viewport);
+    /**
+     * \brief Helper function to move the view
+     *
+     * \param offset Offset to apply to the view
+     */
     void move(const Vector2f& offset);
+    /**
+     * \brief Helper function to rotate the view
+     *
+     * \param angle Angle to apply to the view, in degrees
+     */
     void rotate(float angle);
+    /**
+     * \brief Helper function to zoom the view
+     *
+     * \param factor Zoom factor to apply to the view
+     */
     void zoom(float factor);
 
+    /**
+     * \brief Get the combined transform of the view
+     *
+     * This also apply a orthogonal projection matrix.
+     *
+     * \return A glm::mat4 of the view
+     */
     const glm::mat4& getTransform() const;
     const glm::mat4& getInverseTransform() const;
 
 private:
-    Vector2f g_center;                    //!< Center of the view, in scene coordinates
-    Vector2f g_size;                      //!< Size of the view, in scene coordinates
-    float g_rotation;                     //!< Angle of rotation of the view rectangle, in degrees
-    fge::RectFloat g_factorViewport;      //!< Viewport rectangle, expressed as a factor of the render-target's size
-    mutable glm::mat4 g_transform;        //!< Precomputed projection transform corresponding to the view
-    mutable glm::mat4 g_inverseTransform; //!< Precomputed inverse projection transform corresponding to the view
-    mutable bool g_transformUpdated;      //!< Internal state telling if the transform needs to be updated
-    mutable bool g_invTransformUpdated;   //!< Internal state telling if the inverse transform needs to be updated
+    fge::Vector2f g_center;
+    fge::Vector2f g_size;
+    float g_rotation; //!< Rotation angle of the view, in degrees
+    fge::RectFloat g_factorViewport;
+    mutable glm::mat4 g_transform;
+    mutable glm::mat4 g_inverseTransform;
+    mutable bool g_transformUpdated;
+    mutable bool g_invTransformUpdated;
 };
 
 } // namespace fge
