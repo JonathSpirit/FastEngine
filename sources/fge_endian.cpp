6bcb9bf54b51ec14a59cf096c9ca377c09390b97
GuillaumeG <48102745+JonathSpirit@users.noreply.github.com>
2023-03-01 20:53:14 +0100

    fge_endian: fix cast warning

--- sources/fge_endian.cpp
+++ sources/fge_endian.cpp
@@ -91,12 +91,12 @@ float SwapHostNetEndian_f(float n)
     }
     else
     {
-        uint32_t r;
+        float r;
         static_cast<uint8_t*>(static_cast<void*>(&r))[0] = static_cast<uint8_t*>(static_cast<void*>(&n))[3];
         static_cast<uint8_t*>(static_cast<void*>(&r))[1] = static_cast<uint8_t*>(static_cast<void*>(&n))[2];
         static_cast<uint8_t*>(static_cast<void*>(&r))[2] = static_cast<uint8_t*>(static_cast<void*>(&n))[1];
         static_cast<uint8_t*>(static_cast<void*>(&r))[3] = static_cast<uint8_t*>(static_cast<void*>(&n))[0];
-        return *reinterpret_cast<float*>(&r);
+        return r;
     }
 }
 double SwapHostNetEndian_d(double n)
@@ -109,7 +109,7 @@ double SwapHostNetEndian_d(double n)
     }
     else
     {
-        uint64_t r;
+        double r;
         static_cast<uint8_t*>(static_cast<void*>(&r))[0] = static_cast<uint8_t*>(static_cast<void*>(&n))[7];
         static_cast<uint8_t*>(static_cast<void*>(&r))[1] = static_cast<uint8_t*>(static_cast<void*>(&n))[6];
         static_cast<uint8_t*>(static_cast<void*>(&r))[2] = static_cast<uint8_t*>(static_cast<void*>(&n))[5];
@@ -118,7 +118,7 @@ double SwapHostNetEndian_d(double n)
         static_cast<uint8_t*>(static_cast<void*>(&r))[5] = static_cast<uint8_t*>(static_cast<void*>(&n))[2];
         static_cast<uint8_t*>(static_cast<void*>(&r))[6] = static_cast<uint8_t*>(static_cast<void*>(&n))[1];
         static_cast<uint8_t*>(static_cast<void*>(&r))[7] = static_cast<uint8_t*>(static_cast<void*>(&n))[0];
-        return *reinterpret_cast<double*>(&r);
+        return r;
     }
 }
 
@@ -157,18 +157,18 @@ float SwapEndian_f(float n)
 {
     static_assert(sizeof(float) == 4, "float doesn't have a size of 4 bytes");
 
-    uint32_t r;
+    float r;
     static_cast<uint8_t*>(static_cast<void*>(&r))[0] = static_cast<uint8_t*>(static_cast<void*>(&n))[3];
     static_cast<uint8_t*>(static_cast<void*>(&r))[1] = static_cast<uint8_t*>(static_cast<void*>(&n))[2];
     static_cast<uint8_t*>(static_cast<void*>(&r))[2] = static_cast<uint8_t*>(static_cast<void*>(&n))[1];
     static_cast<uint8_t*>(static_cast<void*>(&r))[3] = static_cast<uint8_t*>(static_cast<void*>(&n))[0];
-    return *reinterpret_cast<float*>(&r);
+    return r;
 }
 double SwapEndian_d(double n)
 {
     static_assert(sizeof(double) == 8, "double doesn't have a size of 8 bytes");
 
-    uint64_t r;
+    double r;
     static_cast<uint8_t*>(static_cast<void*>(&r))[0] = static_cast<uint8_t*>(static_cast<void*>(&n))[7];
     static_cast<uint8_t*>(static_cast<void*>(&r))[1] = static_cast<uint8_t*>(static_cast<void*>(&n))[6];
     static_cast<uint8_t*>(static_cast<void*>(&r))[2] = static_cast<uint8_t*>(static_cast<void*>(&n))[5];
@@ -177,7 +177,7 @@ double SwapEndian_d(double n)
     static_cast<uint8_t*>(static_cast<void*>(&r))[5] = static_cast<uint8_t*>(static_cast<void*>(&n))[2];
     static_cast<uint8_t*>(static_cast<void*>(&r))[6] = static_cast<uint8_t*>(static_cast<void*>(&n))[1];
     static_cast<uint8_t*>(static_cast<void*>(&r))[7] = static_cast<uint8_t*>(static_cast<void*>(&n))[0];
-    return *reinterpret_cast<double*>(&r);
+    return r;
 }
 
 bool IsBigEndian()
