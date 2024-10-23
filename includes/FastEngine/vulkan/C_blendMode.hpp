4ee1a6f72c0023090ccfdf7d37648af6db74e4a1
GuillaumeG. <48102745+JonathSpirit@users.noreply.github.com>
2024-10-24 01:01:52 +0200

    BlendMode: remove extra includes, add docs, add inline on constexpr global variables

--- includes/FastEngine/vulkan/C_blendMode.hpp
+++ includes/FastEngine/vulkan/C_blendMode.hpp
@@ -17,14 +17,22 @@
 #ifndef _FGE_VULKAN_C_BLENDMODE_HPP_INCLUDED
 #define _FGE_VULKAN_C_BLENDMODE_HPP_INCLUDED
 
-#include "FastEngine/fge_extern.hpp"
 #include "volk.h"
-#include <cstdint>
-#include <functional>
 
 namespace fge::vulkan
 {
 
+/**
+ * \file
+ * A file containing an abstraction for vulkan blendmode and basic default blendmode.
+ * \ingroup vulkan
+ * @{
+ */
+
+/**
+ * \struct BlendMode
+ * \brief This is a simple abstraction class for VkBlendFactor, VkBlendOp
+ */
 struct BlendMode
 {
     constexpr BlendMode() noexcept = default;
@@ -69,26 +77,30 @@ struct BlendMode
     VkBlendOp _alphaBlendOp = VK_BLEND_OP_ADD;
 };
 
-constexpr BlendMode BlendAlpha(VK_BLEND_FACTOR_SRC_ALPHA,
-                               VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA,
-                               VK_BLEND_OP_ADD,
-                               VK_BLEND_FACTOR_ONE,
-                               VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA,
-                               VK_BLEND_OP_ADD); //!< Blend source and dest according to dest alpha
-constexpr BlendMode BlendAdd(VK_BLEND_FACTOR_SRC_ALPHA,
-                             VK_BLEND_FACTOR_ONE,
-                             VK_BLEND_OP_ADD,
-                             VK_BLEND_FACTOR_ONE,
-                             VK_BLEND_FACTOR_ONE,
-                             VK_BLEND_OP_ADD); //!< Add source to dest
-constexpr BlendMode
+inline constexpr BlendMode BlendAlpha(VK_BLEND_FACTOR_SRC_ALPHA,
+                                      VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA,
+                                      VK_BLEND_OP_ADD,
+                                      VK_BLEND_FACTOR_ONE,
+                                      VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA,
+                                      VK_BLEND_OP_ADD); //!< Blend source and dest according to dest alpha
+inline constexpr BlendMode BlendAdd(VK_BLEND_FACTOR_SRC_ALPHA,
+                                    VK_BLEND_FACTOR_ONE,
+                                    VK_BLEND_OP_ADD,
+                                    VK_BLEND_FACTOR_ONE,
+                                    VK_BLEND_FACTOR_ONE,
+                                    VK_BLEND_OP_ADD); //!< Add source to dest
+inline constexpr BlendMode
         BlendMultiply(VK_BLEND_FACTOR_DST_COLOR, VK_BLEND_FACTOR_ZERO, VK_BLEND_OP_ADD); //!< Multiply source and dest
-constexpr BlendMode
+inline constexpr BlendMode
         BlendMin(VK_BLEND_FACTOR_ONE, VK_BLEND_FACTOR_ONE, VK_BLEND_OP_MIN); //!< Take minimum between source and dest
-constexpr BlendMode
+inline constexpr BlendMode
         BlendMax(VK_BLEND_FACTOR_ONE, VK_BLEND_FACTOR_ONE, VK_BLEND_OP_MAX); //!< Take maximum between source and dest
-constexpr BlendMode
+inline constexpr BlendMode
         BlendNone(VK_BLEND_FACTOR_ONE, VK_BLEND_FACTOR_ZERO, VK_BLEND_OP_ADD); //!< Overwrite dest with source
+
+/**
+ * @}
+ */
 
 } // namespace fge::vulkan
 
